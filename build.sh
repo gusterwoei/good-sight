789cc7b79fd4cf7f6214207229e14639039b7b34
Gricher <seongwoei.chua@veltra.com>
2018-10-30 15:44:55 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 electron-packager . "Good Sight" --platform=darwin --out=../dist/ --icon=app-icon --overwrite
+rm -rf /Applications/Good\ Sight.app
+mv ../dist/Good\ Sight-darwin-x64/Good\ Sight.app /Applications/Good\ Sight.app
