97779f54cce9080973e66a9b6b79cc24791c7320
Guster Woei <gusterwoei@gmail.com>
2019-01-14 16:13:17 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Note: need to manually install electron-packager globally first
+# npm install -g electron-packager
+
 electron-packager . "Good Sight" --platform=darwin --out=dist/ --icon=app-icon --overwrite
 rm -rf /Applications/Good\ Sight.app
 mv ./dist/Good\ Sight-darwin-x64/Good\ Sight.app /Applications/Good\ Sight.app
